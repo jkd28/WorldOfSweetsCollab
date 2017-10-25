e40efb59baee1f95c890e8547e259b1f53439332
JD <jkd28@pitt.edu>
2017-10-25 12:34:56 -0400

    Add value validation for Card color and int value
    This commit also includes the tests necessary for these
    implementations.

--- src/main/java/Card.java
+++ src/main/java/Card.java
@@ -3,8 +3,8 @@ public class Card{
     private String color; // Can be Red, Blue
 
     public Card(int v, String c){
-        value = v;
-        color = c.toUpperCase();
+        value = validateValue(v);
+        color = validateColor(c);
     }
 
     // Accessors
@@ -14,5 +14,31 @@ public class Card{
 
     public String getColor(){
         return color;
+    }
+
+    // Since 1 and 2 are the only valid input for the value field, an
+    // error is thrown when neither of those 2 is entered
+    private int validateValue(int someValue) throws IllegalArgumentException {
+        if (someValue != 1 && someValue != 2){
+            throw new IllegalArgumentException("Value must be 1 or 2!");
+        } else {
+            return someValue;
+        }
+    }
+
+    // The string provided for color must be one of the valid colors for the game
+    // so an IllegalArgumentException is thrown if the provided string is not
+    // considered a valid color
+    private String validateColor(String someColor) throws IllegalArgumentException {
+        String[] validColors = {"RED", "YELLOW", "BLUE", "GREEN", "ORANGE"};
+        someColor = someColor.toUpperCase();
+
+        for(String valid : validColors){
+            if (valid.equals(someColor)) {
+                return someColor;
+            }
+        }
+        // If we reach here, the string cannot be a vaild color
+        throw new IllegalArgumentException("Color must be a valid color!");
     }
 }
--- src/test/java/CardTest.java
+++ src/test/java/CardTest.java
@@ -8,16 +8,47 @@ public class CardTest {
     // Test that a card properly returns an uppercase
     // string value for the color
     @Test
-    public void testCardGetColor(){
-        testCard = new Card(1, "test");
-        assertEquals("TEST", testCard.getColor());
+    public void testGetColor(){
+        testCard = new Card(1, "RED");
+        assertEquals("RED", testCard.getColor());
     }
 
     // Test that the card properly returns an int value
-    // when the getValue method is called
+    // when the getValue method is called after the card is initialized
+    // with a valid value (1 or 2)
     @Test
-    public void testCardGetValue(){
-        testCard = new Card(1, "test");
+    public void testGetValue(){
+        testCard = new Card(1, "BLUE");
+        Card testCard2 = new Card(2, "YELLOW");
+
         assertEquals(1, testCard.getValue());
+        assertEquals(2, testCard2.getValue());
+    }
+
+    // Test that when an invalid value is passed to the card constructor,
+    // an IllegalArgumentException is thrown.  This ensures no invalid value
+    // (anything other than 1 or 2) can be inserted into a card
+    @Test
+    public void testInvalidValue(){
+        int invalidValue = 132434;
+        String validColor = "red";
+        try {
+            testCard = new Card(invalidValue, validColor);
+            fail();
+        } catch (IllegalArgumentException e) {
+            // Test passes if we get the error thrown
+        }
+    }
+
+    @Test
+    public void testIllegalColor(){
+        int validValue = 1;
+        String invalidColor = "this should throw an error";
+        try {
+            testCard = new Card(validValue, invalidColor);
+            fail();
+        } catch (IllegalArgumentException e) {
+            // Test passes if we get the error thrown
+        }
     }
 }
