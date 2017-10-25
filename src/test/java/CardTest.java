ad8213f02422cdc781b1ca80846d40890701fde5
JD <jkd28@pitt.edu>
2017-10-25 12:02:54 -0400

    Add tests for Deck drawing/reshuffling
    Also add comment annotations to existing tests to describe what
    they are testing.

--- src/test/java/CardTest.java
+++ src/test/java/CardTest.java
@@ -4,12 +4,17 @@ import static org.junit.Assert.*;
 public class CardTest {
     Card testCard;
 
+
+    // Test that a card properly returns an uppercase
+    // string value for the color
     @Test
     public void testCardGetColor(){
         testCard = new Card(1, "test");
         assertEquals("TEST", testCard.getColor());
     }
 
+    // Test that the card properly returns an int value
+    // when the getValue method is called
     @Test
     public void testCardGetValue(){
         testCard = new Card(1, "test");
--- src/test/java/DeckTest.java
+++ src/test/java/DeckTest.java
@@ -2,5 +2,30 @@ import org.junit.*;
 import static org.junit.Assert.*;
 
 public class DeckTest {
-    
+    Deck testDeck;
+
+    @Before
+    public void setup(){
+        testDeck = new Deck();
+    }
+
+    // The deck of cards contains 60 cards.  After drawing all
+    // cards, the deck needs to reshuffule and should never return a null.
+    // This test assures that the deck reshuffles, and will fail if a
+    // NullPointerException is thrown or a null is returned from the draw method.
+    @Test
+    public void testDeckReshuffle(){
+        for(int i = 0; i < 60; i++){
+            testDeck.draw();
+        }
+        Card testCard = testDeck.draw();
+        assertNotNull(testCard);
+    }
+
+    // Test that a card is returned when the draw method is called on the deck 
+    @Test
+    public void testDraw(){
+        Card testCard = testDeck.draw();
+        assertNotNull(testCard);
+    }
 }
