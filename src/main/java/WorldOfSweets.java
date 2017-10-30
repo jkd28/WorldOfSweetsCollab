7867ac3c57ed2a6d4815a2697ec5052ac1feca56
Benjamin J Muscato <benjamin.muscato@pitt.edu>
2017-10-30 10:07:38 -0400

    JOptionPane now asks for number of players
    To get the number of players for a new game of 'World of Sweets',
    JOptionPane will prompt the user, rather than using command-line args.

--- src/main/java/WorldOfSweets.java
+++ src/main/java/WorldOfSweets.java
@@ -1,3 +1,6 @@
+import javax.swing.JOptionPane;
+import javax.swing.JFormattedTextField;
+
 public class WorldOfSweets {
 
 	// Basic game information
@@ -5,30 +8,73 @@ public class WorldOfSweets {
 												//		It should probably eventually get moved to whatever "GameplayDriver" sort of class
 												//		that gets made.
 
-    private static void showInvalidInputErrorMessage() {
-		System.out.println("Usage: java WorldOfSweets <numPlayers>");
-		System.out.println("'numPlayers' must be a positive integer >= " + MIN_PLAYERS);
-		System.exit(1);
+  	private static int getNumPlayersFromUser(){
+    	int numPlayers = -1;
+
+    	// --------------------------------------- //
+    	// Get the number of players from the user //
+    	// --------------------------------------- //
+    	while(true){
+			// Ask the user for the number of players
+			String result = JOptionPane.showInputDialog(null, 
+				"Welcome to 'World of Sweets'!\nHow many players will be in this game? (minimum "+MIN_PLAYERS+" players)", 
+				"Welcome!",
+				JOptionPane.QUESTION_MESSAGE);
+	    	
+	    	// If the user hits "Cancel" or the Exit button, quit the program
+	    	if(result == null){
+	    		JOptionPane.showMessageDialog(null, "Goodbye!");
+	    		System.exit(0);
+	    	}
+
+	    	// Try parsing the user input into a number, and make sure it's >= MIN_PLAYERS
+	    	try{
+	    		numPlayers = Integer.parseInt(result);
+	    		if(numPlayers < MIN_PLAYERS){
+	    			JOptionPane.showMessageDialog(null, 
+	    				"You have to have at least " + MIN_PLAYERS + " players for 'World of Sweets'!",
+	    				"Too Few Players",
+	    				JOptionPane.ERROR_MESSAGE);
+	    			continue;
+	    		}
+	    		break;
+	    	}
+	    	catch(NumberFormatException e){
+	    		JOptionPane.showMessageDialog(null, 
+	    			"I'm sorry, but \"" + result + "\" is not a valid number.", 
+	    			"Invalid Input", 
+	    			JOptionPane.ERROR_MESSAGE);
+	    	}
+	    	catch(Exception e){
+	    		JOptionPane.showMessageDialog(null, 
+	    			"An unknown error has occurred, please try again.", 
+	    			"Unknown Error", 
+	    			JOptionPane.ERROR_MESSAGE);
+	    	}
+    	}
+
+	    return numPlayers;
     }
     
     public static void main(String[] args) {
-		// ------------------------ //
-		// Validate input arguments //
-		// ------------------------ //
-		if (args.length != 1) {
-		    showInvalidInputErrorMessage();
-		}
+    	// ------------------------- //
+    	// Get the number of Players //
+    	// ------------------------- //
+    	int numPlayers = getNumPlayersFromUser();
 
-		int numPlayers = -1;
-		try {
-		    numPlayers = Integer.parseInt(args[0]);
-		    if(numPlayers < MIN_PLAYERS){
-		    	showInvalidInputErrorMessage();
-		    }
-		} catch (Exception ex) {
-		    showInvalidInputErrorMessage();
-		}
-		
+    	// Validate that we got a proper response
+    	if(numPlayers == -1){
+    		System.err.println("An unexpected error occurred; exiting program.");
+    		System.exit(1);
+    	}
+    	else if(numPlayers < MIN_PLAYERS){
+    		System.err.println("You cannot play 'World of Sweets' with less than "+MIN_PLAYERS+" players; exiting program.");
+    		System.exit(1);
+    	}
+    	
+
+	    System.out.println("Starting a new game of 'World of Sweets' with " + numPlayers + " players...");
+
 
 		// --------------------------------------------------- //
 		// Create the "World of Sweets" GUI and start the game //
