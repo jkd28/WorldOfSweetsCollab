7e9aecc1763648fc3ed07a852ef49ccf60a8d8c8
JD <jkd28@pitt.edu>
2017-11-02 14:58:31 -0400

    Add unit tests for new Skip Card implementation

--- src/test/java/DeckTest.java
+++ src/test/java/DeckTest.java
@@ -15,17 +15,35 @@ public class DeckTest {
     // NullPointerException is thrown or a null is returned from the draw method.
     @Test
     public void testDeckReshuffle(){
-        for(int i = 0; i < 60; i++){
+        int deckSize = testDeck.size();
+        for(int i = 0; i < deckSize; i++){
             testDeck.draw();
         }
+        // draw the last card from the deck
         Card testCard = testDeck.draw();
         assertNotNull(testCard);
     }
 
-    // Test that a card is returned when the draw method is called on the deck 
+    // Test that a card is returned when the draw method is called on the deck
     @Test
     public void testDraw(){
         Card testCard = testDeck.draw();
         assertNotNull(testCard);
+    }
+
+    // Test that exactly 5 skip cards are present in a new deck
+    @Test
+    public void testNumSkipCards(){
+        int skipCount = 0;
+        int deckSize = testDeck.size();
+
+        for (int i = 0; i < deckSize; i++){
+            Card drawn = testDeck.draw();
+            if (drawn.getValue() == 0) {
+                skipCount++;
+            }
+        }
+
+        assertEquals(5, skipCount);
     }
 }
