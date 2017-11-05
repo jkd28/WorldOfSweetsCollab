6ee40e2e2fa915691463bbf7f9569f14f6d0efc2
Brian Knotten <bjk69@pitt.edu>
2017-11-04 21:29:11 -0400

    BJK implement middle square label and unit test.

--- src/main/java/BoardPanel.java
+++ src/main/java/BoardPanel.java
@@ -11,40 +11,50 @@ public class BoardPanel extends JPanel{
     private JPanel[] spaces;
     private JLabel start = new JLabel("Start ->");
     private JLabel grandma = new JLabel("-> Grandma's House");
+    private JLabel midLabel = new JLabel("MID");
     private static final Font BUTTON_FONT = new Font("Courier", Font.PLAIN, 48);
-    private static final int BOARD_COLUMNS = 10;
-    private static final int BOARD_ROWS = 7;
+    private static final int BOARD_COLUMNS = 5;
+    private static final int BOARD_ROWS = 5;
     private static final int BOARD_HORIZONTAL_GAP = 20;
     private static final int BOARD_VERTICAL_GAP = 10;
     private static final GridLayout BOARD_LAYOUT = new GridLayout(BOARD_ROWS, BOARD_COLUMNS, BOARD_VERTICAL_GAP, BOARD_HORIZONTAL_GAP);
+    private static final int NUM_SPACES = BOARD_COLUMNS * BOARD_ROWS;
+    private static final int MID_SPACE = (NUM_SPACES / 2); // The middle space. When even # spaces, the one closer to the final space
     
     public BoardPanel(Player[] players){
 	setLayout(BOARD_LAYOUT);
 
-	// Add the start and end (grandma's house) spaces
-	spaces = new JPanel[BOARD_COLUMNS * BOARD_ROWS];
+	spaces = new JPanel[NUM_SPACES];
 	spaces[0] = new JPanel();
+
+	// Add the player names
 	if(players.length > 0){
-		String labelText = start.getText() + " (";
-		for(Player player : players){
-			labelText = labelText + player.getName() + ", ";
-		}
-		labelText = labelText.substring(0, labelText.length() - 2);
-		labelText = labelText + ")";
-		spaces[0].add(new JLabel(labelText));
+	    String labelText = start.getText() + " (";
+	    for(Player player : players){
+		labelText = labelText + player.getName() + ", ";
+	    }
+	    labelText = labelText.substring(0, labelText.length() - 2);
+	    labelText = labelText + ")";
+	    spaces[0].add(new JLabel(labelText));
 	}
 	else{
-		spaces[0].add(start);
+	    spaces[0].add(start);
 	}
+
+	// Add the start space
 	spaces[0].setBorder(BorderFactory.createLineBorder(Color.black));
-	spaces[1] = new JPanel();
-	spaces[1].add(grandma);
-	spaces[1].setBorder(BorderFactory.createLineBorder(Color.black));
 	add(spaces[0]);
 
-	// Add all of the colored spaces to the board
-	for(int i = 2; i < spaces.length; i++){
-	    spaces[i] = new JPanel();
+	// Add all of the colored spaces tpo the board
+	for(int i = 1; i < spaces.length-1; i++){
+	    // Mark the middle space
+	    if (i == MID_SPACE){
+		spaces[i] = new JPanel(new GridBagLayout());
+		spaces[i].add(midLabel);
+	    } else {
+		spaces[i] = new JPanel();
+	    }
+	    
 	    if (i % 5 == 2){
 		spaces[i].setBackground(Color.RED);
 	    }else if (i % 5 == 3){
@@ -60,12 +70,21 @@ public class BoardPanel extends JPanel{
 	   	    
 	    add(spaces[i]);
 	}
-	add(spaces[1]);
 
+	// Add the end (grandma's house) space
+	spaces[NUM_SPACES-1] = new JPanel();
+	spaces[NUM_SPACES-1].add(grandma);
+	spaces[NUM_SPACES-1].setBorder(BorderFactory.createLineBorder(Color.black));
+	add(spaces[NUM_SPACES-1]);
     }
 
     // Retrieve a specific board space
     public JPanel getSpace(int index){
 	return spaces[index];
+    }
+
+    // Retrieve the number of spaces 
+    public int getNumSpaces(){
+	return spaces.length;
     }
 }
