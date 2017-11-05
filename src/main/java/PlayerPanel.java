baece0b78cbd80f879fb38f345dcf5d1b3fe510f
Brian Knotten <bjk69@pitt.edu>
2017-11-05 00:42:12 -0400

    BJK Mostly working reformatting

--- src/main/java/PlayerPanel.java
+++ src/main/java/PlayerPanel.java
@@ -5,26 +5,50 @@ import javax.swing.*;
 public class PlayerPanel extends JPanel{
     private JPanel currPanel = new JPanel(new GridBagLayout());
     private JPanel nextPanel = new JPanel(new GridBagLayout());
+    private GridBagLayout gridBag = new GridBagLayout();
+    private JPanel orderPanel = new JPanel(gridBag);
     private JLabel currDisplayText = new JLabel();
     private JLabel nextDisplayText = new JLabel();
+    private JLabel ordDisplayText = new JLabel();
     private final String START_TEXT = "First turn!";
     private final String CURR_PRE_TEXT = "Current Player: \n";
     private final String NEXT_PRE_TEXT = "Next Player: \n";
+    private final String ORD_PRE_TEXT = "Player Order: \n";
+    private GridBagConstraints c = new GridBagConstraints();
 
-    public PlayerPanel(Player p){
+    public PlayerPanel(Player[] players){
         setLayout(new GridLayout(3, 1));
 
+	// Set the initial text of each JPanel
 	currDisplayText.setText(START_TEXT);
-	nextDisplayText.setText(NEXT_PRE_TEXT + p.getName());
-	
+	nextDisplayText.setText(NEXT_PRE_TEXT + players[0].getName());
+	ordDisplayText.setText(ORD_PRE_TEXT);
+
+    	c.anchor = GridBagConstraints.WEST; // Force JLabels to align left
+	c.gridy = 0; // Place the first JLabel in the first row
+	orderPanel.add(ordDisplayText, c);
+
+	// Add each player's name to the turn order
+	for (int i = 0; i < players.length; i++){
+	    JLabel tempLabel = new JLabel((i+1) + " " + players[i].getName());
+	    c.gridy = i+1; // Place each JLabel in the next row successively 
+	    orderPanel.add(tempLabel, c);
+	}
+
+	// Add the JLabels to current and next player JPanels
 	currPanel.add(currDisplayText);
 	nextPanel.add(nextDisplayText);
 
+	// Add the JPanels to the main JPanel
     	add(currPanel);
 	add(nextPanel);
+	add(orderPanel);
     }
 
-    public void changePlayer(Player newPlayer){
-        nextDisplayText.setText(NEXT_PRE_TEXT + newPlayer.getName());
+    // Change the current and next players
+    public void changePlayer(Player[] players, int nextPlayer){
+	
+	currDisplayText.setText(CURR_PRE_TEXT + players[nextPlayer-1].getName());
+        nextDisplayText.setText(NEXT_PRE_TEXT + players[nextPlayer].getName());
     }
 }
