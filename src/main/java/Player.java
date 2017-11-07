776e8fa5aeafcdbba0ef6fd0a5026753634e3c07
Wyatt Borsos <wab32@pitt.edu>
2017-11-07 18:32:22 -0500

    Removed unneeded Player methods get/set color, changed position to a boardspace object

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -1,6 +1,6 @@
 public class Player{
     private String name;
-    private int position = 0; //all players start at "0th" position according to write up
+    private BoardSpace position = null; //all players start at "0th" position according to write up
     private String posColor; //
     private int token;//what should this be? int doesn't feel right
     private int wins = 0;
@@ -13,17 +13,17 @@ public class Player{
         name = n;
     }
 
-    public boolean Move(int i, String c){
-        setPosition(i);
-        setposColor(c);
+    public boolean Move(BoardSpace bs){
+        setPosition(bs);
+        // setposColor(bs.Color);
         return true;
     }
 
-    public int getPosition(){
+    public BoardSpace getPosition(){
         return position;
     }
-    public boolean setPosition(int i){
-        position = i;
+    public boolean setPosition(BoardSpace bs){
+        position = bs;
         return true;
     }
 
@@ -43,13 +43,13 @@ public class Player{
         return true;
     }
 
-    public String getposColor(){
-        return posColor;
-    }
-    public boolean setposColor(String c){
-        posColor = c;
-        return true;
-    }
+    // public String getposColor(){
+    //     return posColor;
+    // }
+    // public boolean setposColor(String c){
+    //     posColor = c;
+    //     return true;
+    // }
 
     public int getWins(){
         return wins;
