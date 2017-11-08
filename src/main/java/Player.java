c6d633d5b22e9452cb038029e180d1f8d27a3aea
Wyatt Borsos <wab32@pitt.edu>
2017-11-07 21:46:19 -0500

    stripped down playter some more

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -1,21 +1,9 @@
 public class Player{
     private String name;
     private BoardSpace position = null; //all players start at "0th" position according to write up
-    private int token;                  //what should this be? int doesn't feel right
-    private int wins = 0;
 
-    public Player(String n, int t){
-        name = n;
-        token = t;
-    }
     public Player(String n){
         name = n;
-    }
-
-    public boolean Move(BoardSpace bs){
-        setPosition(bs);
-        // setposColor(bs.Color);
-        return true;
     }
 
     //Use an instance of a boardspace object as position    
@@ -34,31 +22,6 @@ public class Player{
     }
     public boolean setName(String n){
         name = n;
-        return true;
-    }
-
-    public int getToken(){
-        return token;
-    }
-    public boolean setToken(int t){
-        token = t;
-        return true;
-    }
-
-    // public String getposColor(){
-    //     return posColor;
-    // }
-    // public boolean setposColor(String c){
-    //     posColor = c;
-    //     return true;
-    // }
-
-    public int getWins(){
-        return wins;
-    }
-    //shouldn't need to add more then 1 win at a time...
-    public boolean addWin(){
-        wins++;
         return true;
     }
 }
