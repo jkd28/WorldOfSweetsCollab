5c4134e427db23d4829336260a043911d53303f4
Benjamin Muscato <BenjaminMuscato@users.noreply.github.com>
2017-11-15 14:10:40 -0500

    Can select .ser files, and .ser files only, now.

--- src/main/java/WorldOfSweets.java
+++ src/main/java/WorldOfSweets.java
@@ -1,5 +1,10 @@
-import javax.swing.JOptionPane;
+import java.io.File;
 import java.io.Serializable;
+import javax.swing.Action;
+import javax.swing.filechooser.FileNameExtensionFilter;
+import javax.swing.JFileChooser;
+import javax.swing.JOptionPane;
+import javax.swing.plaf.basic.BasicFileChooserUI;
 
 public class WorldOfSweets implements Serializable {
 
@@ -46,7 +51,7 @@ public class WorldOfSweets implements Serializable {
     	// ===================================================== //
     	// Ask if the user wants to load a previously saved game //
     	// ===================================================== //
-    	int result = JOptionPane.showConfirmDialog(
+    	int loadPreviousGameResult = JOptionPane.showConfirmDialog(
     		null,
     		"Would you like to load a previously saved game?",
     		"Load a Saved Game?",
@@ -56,7 +61,7 @@ public class WorldOfSweets implements Serializable {
     	// ============================================== //
     	// If user CANCELLED the window, exit the program //
     	// ============================================== //
-    	if(result == JOptionPane.CLOSED_OPTION){
+    	if(loadPreviousGameResult == JOptionPane.CLOSED_OPTION){
     		JOptionPane.showMessageDialog(null, "Goodbye!", "Goodbye!", JOptionPane.INFORMATION_MESSAGE);
     		System.exit(0);
     	}
@@ -64,9 +69,68 @@ public class WorldOfSweets implements Serializable {
     	// ============================================================================================= //
     	// Else if the user wants to load a game, have them choose a save-file to load, and then load it //
     	// ============================================================================================= //
-    	else if(result == JOptionPane.YES_OPTION){
-    		JOptionPane.showMessageDialog(null, "We're sorry, but this feature is not yet available. Goodbye!", "Feature Unavailable", JOptionPane.ERROR_MESSAGE);
-    		System.exit(0);
+    	else if(loadPreviousGameResult == JOptionPane.YES_OPTION){
+    		// ---------------------------------- //
+    		// Ask the user to select a save file //
+    		// ---------------------------------- //
+    		// Create the FileChooser
+    		File workingDirectory = new File(System.getProperty("user.dir"));
+    		FileNameExtensionFilter fileFilter = new FileNameExtensionFilter("WorldOfSweets Save Files (*.ser)", "ser"); // By convention, serialized objects in Java are stored in ".ser" files
+    		
+    		JFileChooser fileChooser = new JFileChooser();
+    		fileChooser.setAcceptAllFileFilterUsed(false);
+    		fileChooser.setCurrentDirectory(workingDirectory);
+    		fileChooser.setDialogType(JFileChooser.OPEN_DIALOG);
+    		fileChooser.setFileFilter(fileFilter);
+    		fileChooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
+    		fileChooser.setMultiSelectionEnabled(false);
+
+    		// Disable the "New Folder" ability in the JFileChooser
+    		BasicFileChooserUI ui = (BasicFileChooserUI)fileChooser.getUI();
+    		Action folder = ui.getNewFolderAction();
+        	folder.setEnabled(false);
+
+    		// Get a result from the FileChooser
+    		File saveFile = null;
+    		while(true){
+    			int fileChooserResult = fileChooser.showOpenDialog(null);
+
+	    		// Check the result from the FileChooser
+	    		if(fileChooserResult == JFileChooser.CANCEL_OPTION){
+	    			JOptionPane.showMessageDialog(null, "Goodbye!", "Goodbye!", JOptionPane.INFORMATION_MESSAGE);
+	    			System.exit(0);
+	    		}
+	    		else if(fileChooserResult == JFileChooser.APPROVE_OPTION){
+	    			saveFile = fileChooser.getSelectedFile();
+
+	    			if(saveFile.isFile() && saveFile.getName().endsWith(".ser")){
+	    				break;
+	    			}
+	    			else{
+	    				JOptionPane.showMessageDialog(null, "I'm sorry, but \""+saveFile.getName()+"\" is not a valid WorldOfSweets save file name.");
+	    				continue;
+	    			}
+	    		}
+	    		else{ //Else we assume that an error occurred
+	    			JOptionPane.showMessageDialog(
+    					null, 
+    					"An unexpected error occurred while trying to open a save-file; exiting program.", 
+    					"Exiting Program", 
+    					JOptionPane.ERROR_MESSAGE
+	    			);
+	    			System.exit(1);
+	    		}
+    		}
+
+    		// --------------------------------------------------------- //
+    		// Try to "Open" the selected save file and resume that game //
+    		// --------------------------------------------------------- //
+    		if(saveFile != null){
+    			System.exit(0);
+    		}
+    		else{
+    			System.exit(1);
+    		}
     	}
 
     	// =========================== //
