ac34a9eadc7a62aa43a0134e8560c741929bef2c
Kevin Good <zzffcrz@gmail.com>
2017-11-18 17:36:33 -0500

    increased font

--- src/main/java/TimerPanel.java
+++ src/main/java/TimerPanel.java
@@ -4,14 +4,16 @@ import javax.swing.*;
 
 public class TimerPanel extends JPanel{
     public TimerPanel(){
+        //put in a thread as per project instructions
         Thread myThread = new Thread(() -> {
             Timer timer = new Timer();
-            JLabel label = new JLabel(timer.getRealTime());
+            JLabel label = new JLabel("Game Time: " + timer.getRealTime());
+            label.setFont(new Font("Dialog", Font.BOLD, 20));
             add(label);
             while (true){
                 timer.updateSeconds();
                 timer.setRealTime(timer.updateTime());
-                label.setText(timer.getRealTime());
+                label.setText("Game Time: " + timer.getRealTime());
                 try{
                     Thread.sleep(timer.SEC_IN_MS);
                 }catch(Exception e){
