23a736d85198e194f28f82f8f7e033d38063a609
JD <jkd28@pitt.edu>
2017-11-20 14:28:33 -0500

    Refactor PlayerPanelTest
    Split the single test in PlayerPanelTest into two individual tests, which
    is more "unit"-like.  The test originally tested two different portions of the player
    panel, so it makes a bit more sense to split them up.

--- src/test/java/PlayerPanelTest.java
+++ src/test/java/PlayerPanelTest.java
@@ -19,24 +19,30 @@ public class PlayerPanelTest{
 
     // The panel should initialize with the message "First turn!"
     // instead of a current player, the first player's name listed
-    // as the next player, and a list of player names in turn order.
+    // as the next player
     @Test
-    public void testInitialPlayerPanel(){
+    public void testInitialPlayerPanelText(){
         String testCurrPlayerMessage = "First turn!";
         String testNextPlayerMessage = "Next Player: \n" + testPlayers[0].getName();
+
+        assertEquals(testCurrPlayerMessage, testPlayerPanel.getCurrPlayerText());
+        assertEquals(testNextPlayerMessage, testPlayerPanel.getNextPlayerText());
+    }
+
+    // Test that the PlayerPanel correctly initializes a list of Strings
+    // which comprises the player order for the game instance
+    @Test
+    public void testPlayerOrder(){
+        // testPlayOrder is of variable length in case the test cases ever need to change in the future
         String testPlayOrder[] = new String[testPlayers.length+1];
         String actualPlayOrder[] = testPlayerPanel.getOrderText();
 
         testPlayOrder[0] = "Player Order: \n";
+        // Because of the variable length, looping is necessary here
         for (int i = 1; i <= testPlayers.length; i++){
             testPlayOrder[i] = i + ") " + testPlayers[i-1].getName();
         }
 
-        assertEquals(testCurrPlayerMessage, testPlayerPanel.getCurrPlayerText());
-        assertEquals(testNextPlayerMessage, testPlayerPanel.getNextPlayerText());
-
-        for (int i = 0; i <= testPlayers.length; i++){
-            assertEquals(testPlayOrder[i], actualPlayOrder[i]);
-        }
+        assertArrayEquals(testPlayOrder, actualPlayOrder);
     }
 }
