dbbb4b0e72999bf91a3b8a4c4423ddb3a2b27678
JD <jkd28@pitt.edu>
2017-11-20 14:11:48 -0500

    Fix whitespace and indentation

--- src/test/java/DeckPanelTest.java
+++ src/test/java/DeckPanelTest.java
@@ -1,6 +1,6 @@
 /*
- * @author Brian Knotten (Github: "BK874"). Primary author of this file, used originally in the "BitsPlease" repository.
- */
+* @author Brian Knotten (Github: "BK874"). Primary author of this file, used originally in the "BitsPlease" repository.
+*/
 import org.junit.*;
 import static org.junit.Assert.*;
 import java.awt.Color;
@@ -10,25 +10,25 @@ public class DeckPanelTest{
 
     @Before
     public void setup(){
-		testDeckPanel = new DeckPanel();
+        testDeckPanel = new DeckPanel();
     }
 
     // The panel should initialize displaying a blank (white) space
     // where the current card will be displayed.
     @Test
     public void testInitialDeckPanelColor(){
-		Color testColor = testDeckPanel.getCurrentColor();
-		assertEquals(testColor, DeckPanel.DEFAULT_COLOR);
+        Color testColor = testDeckPanel.getCurrentColor();
+        assertEquals(testColor, DeckPanel.DEFAULT_COLOR);
     }
 
     // Test that a correct color is displayed after clicking the draw button
     @Test
     public void testDeckPanelColor(){
-		testDeckPanel.getDrawButton().doClick();
+        testDeckPanel.getDrawButton().doClick();
 
-		Color testColor = testDeckPanel.getCurrentColor();
+        Color testColor = testDeckPanel.getCurrentColor();
 
-		assertEquals(true, Card.isValidColor(testColor));
+        assertEquals(true, Card.isValidColor(testColor));
     }
 
     // After the draw button is click 60 times there each color should
@@ -36,96 +36,96 @@ public class DeckPanelTest{
     // double card, with no other color having been displayed.
     @Test
     public void testDeckPanelDraw(){
-		int redCounter,red2Counter, yellowCounter, yellow2Counter,
-		    blueCounter, blue2Counter, greenCounter,green2Counter,
-		    orangeCounter, orange2Counter, skipCounter, specialCounter,
-		    wrongCounter, total;
-		Color testColor;
-		Card testCard;
-		int testValue;
-		redCounter = red2Counter = yellowCounter = yellow2Counter =
-		    blueCounter = blue2Counter = greenCounter = green2Counter =
-		    orangeCounter = orange2Counter = skipCounter = specialCounter =
-		    wrongCounter = 0;
+        int redCounter,red2Counter, yellowCounter, yellow2Counter,
+        blueCounter, blue2Counter, greenCounter,green2Counter,
+        orangeCounter, orange2Counter, skipCounter, specialCounter,
+        wrongCounter, total;
+        Color testColor;
+        Card testCard;
+        int testValue;
+        redCounter = red2Counter = yellowCounter = yellow2Counter =
+        blueCounter = blue2Counter = greenCounter = green2Counter =
+        orangeCounter = orange2Counter = skipCounter = specialCounter =
+        wrongCounter = 0;
 
-		int numCardsInDeck = testDeckPanel.getDrawDeck().size();
+        int numCardsInDeck = testDeckPanel.getDrawDeck().size();
 
-		for (int i = 0; i < numCardsInDeck; i++){
-		    testDeckPanel.getDrawButton().doClick();
-		    testCard = testDeckPanel.getCurrentCard();
-		    testColor = testDeckPanel.getCurrentColor();
-		    testValue = testCard.getValue();
+        for (int i = 0; i < numCardsInDeck; i++){
+            testDeckPanel.getDrawButton().doClick();
+            testCard = testDeckPanel.getCurrentCard();
+            testColor = testDeckPanel.getCurrentColor();
+            testValue = testCard.getValue();
 
 
-		    if(testValue == Card.SKIP){
-		    	skipCounter++;
-		    }
-		    else if(testValue >= Card.GO_TO_FIRST_SPECIAL){
-		    	specialCounter++;
-		    }
-		    else if(testValue == Card.SINGLE){
-		    	if(testColor.equals(Color.RED)){
-		    		redCounter++;
-		    	}
-		    	else if(testColor.equals(Color.YELLOW)){
-		    		yellowCounter++;
-		    	}
-		    	else if(testColor.equals(Color.BLUE)){
-		    		blueCounter++;
-		    	}
-		    	else if(testColor.equals(Color.GREEN)){
-		    		greenCounter++;
-		    	}
-		    	else if(testColor.equals(Color.ORANGE)){
-		    		orangeCounter++;
-		    	}
-		    	else{
-		    		wrongCounter++;
-		    	}
-		    }
-		    else if(testValue == Card.DOUBLE){
-		    	if(testColor.equals(Color.RED)){
-		    		red2Counter++;
-		    	}
-		    	else if(testColor.equals(Color.YELLOW)){
-		    		yellow2Counter++;
-		    	}
-		    	else if(testColor.equals(Color.BLUE)){
-		    		blue2Counter++;
-		    	}
-		    	else if(testColor.equals(Color.GREEN)){
-		    		green2Counter++;
-		    	}
-		    	else if(testColor.equals(Color.ORANGE)){
-		    		orange2Counter++;
-		    	}
-		    	else{
-		    		wrongCounter++;
-		    	}
-		    }
-		    else{
-		    	wrongCounter++;
-		    }
+            if(testValue == Card.SKIP){
+                skipCounter++;
+            }
+            else if(testValue >= Card.GO_TO_FIRST_SPECIAL){
+                specialCounter++;
+            }
+            else if(testValue == Card.SINGLE){
+                if(testColor.equals(Color.RED)){
+                    redCounter++;
+                }
+                else if(testColor.equals(Color.YELLOW)){
+                    yellowCounter++;
+                }
+                else if(testColor.equals(Color.BLUE)){
+                    blueCounter++;
+                }
+                else if(testColor.equals(Color.GREEN)){
+                    greenCounter++;
+                }
+                else if(testColor.equals(Color.ORANGE)){
+                    orangeCounter++;
+                }
+                else{
+                    wrongCounter++;
+                }
+            }
+            else if(testValue == Card.DOUBLE){
+                if(testColor.equals(Color.RED)){
+                    red2Counter++;
+                }
+                else if(testColor.equals(Color.YELLOW)){
+                    yellow2Counter++;
+                }
+                else if(testColor.equals(Color.BLUE)){
+                    blue2Counter++;
+                }
+                else if(testColor.equals(Color.GREEN)){
+                    green2Counter++;
+                }
+                else if(testColor.equals(Color.ORANGE)){
+                    orange2Counter++;
+                }
+                else{
+                    wrongCounter++;
+                }
+            }
+            else{
+                wrongCounter++;
+            }
 
-		}
-		total = redCounter + red2Counter + yellowCounter + yellow2Counter +
-		    blueCounter + blue2Counter + greenCounter + green2Counter +
-		    orangeCounter + orange2Counter + skipCounter + specialCounter +
-		    wrongCounter;
+        }
+        total = redCounter + red2Counter + yellowCounter + yellow2Counter +
+        blueCounter + blue2Counter + greenCounter + green2Counter +
+        orangeCounter + orange2Counter + skipCounter + specialCounter +
+        wrongCounter;
 
-		assertEquals(0, wrongCounter);
-		assertEquals(Deck.NUM_SINGLE_CARDS_PER_COLOR, redCounter);
-		assertEquals(Deck.NUM_SINGLE_CARDS_PER_COLOR, yellowCounter);
-		assertEquals(Deck.NUM_SINGLE_CARDS_PER_COLOR, blueCounter);
-		assertEquals(Deck.NUM_SINGLE_CARDS_PER_COLOR, greenCounter);
-		assertEquals(Deck.NUM_SINGLE_CARDS_PER_COLOR, orangeCounter);
-		assertEquals(Deck.NUM_DOUBLE_CARDS_PER_COLOR, red2Counter);
-		assertEquals(Deck.NUM_DOUBLE_CARDS_PER_COLOR, yellow2Counter);
-		assertEquals(Deck.NUM_DOUBLE_CARDS_PER_COLOR, blue2Counter);
-		assertEquals(Deck.NUM_DOUBLE_CARDS_PER_COLOR, green2Counter);
-		assertEquals(Deck.NUM_DOUBLE_CARDS_PER_COLOR, orange2Counter);
-		assertEquals(Deck.NUM_GO_TO_CARDS * 5, specialCounter);
-		assertEquals(Deck.NUM_SKIP_CARDS, skipCounter);
-		assertEquals(numCardsInDeck, total);
+        assertEquals(0, wrongCounter);
+        assertEquals(Deck.NUM_SINGLE_CARDS_PER_COLOR, redCounter);
+        assertEquals(Deck.NUM_SINGLE_CARDS_PER_COLOR, yellowCounter);
+        assertEquals(Deck.NUM_SINGLE_CARDS_PER_COLOR, blueCounter);
+        assertEquals(Deck.NUM_SINGLE_CARDS_PER_COLOR, greenCounter);
+        assertEquals(Deck.NUM_SINGLE_CARDS_PER_COLOR, orangeCounter);
+        assertEquals(Deck.NUM_DOUBLE_CARDS_PER_COLOR, red2Counter);
+        assertEquals(Deck.NUM_DOUBLE_CARDS_PER_COLOR, yellow2Counter);
+        assertEquals(Deck.NUM_DOUBLE_CARDS_PER_COLOR, blue2Counter);
+        assertEquals(Deck.NUM_DOUBLE_CARDS_PER_COLOR, green2Counter);
+        assertEquals(Deck.NUM_DOUBLE_CARDS_PER_COLOR, orange2Counter);
+        assertEquals(Deck.NUM_GO_TO_CARDS * 5, specialCounter);
+        assertEquals(Deck.NUM_SKIP_CARDS, skipCounter);
+        assertEquals(numCardsInDeck, total);
     }
 }
--- src/test/java/PlayerPanelTest.java
+++ src/test/java/PlayerPanelTest.java
@@ -1,6 +1,6 @@
 /*
- * @author Brian Knotten (Github: "BK874"). Primary author of this file, used originally in the "BitsPlease" repository.
- */
+* @author Brian Knotten (Github: "BK874"). Primary author of this file, used originally in the "BitsPlease" repository.
+*/
 import org.junit.*;
 import static org.junit.Assert.*;
 
@@ -10,11 +10,11 @@ public class PlayerPanelTest{
 
     @Before
     public void setup(){
-	testPlayers[0] = new Player("Benjamin");
-	testPlayers[1] = new Player("Brian");
-	testPlayers[2] = new Player("John");
-	testPlayers[3] = new Player("Kevin");
-	testPlayerPanel = new PlayerPanel(testPlayers);
+        testPlayers[0] = new Player("Benjamin");
+        testPlayers[1] = new Player("Brian");
+        testPlayers[2] = new Player("John");
+        testPlayers[3] = new Player("Kevin");
+        testPlayerPanel = new PlayerPanel(testPlayers);
     }
 
     // The panel should initialize with the message "First turn!"
@@ -22,21 +22,21 @@ public class PlayerPanelTest{
     // as the next player, and a list of player names in turn order.
     @Test
     public void testInitialPlayerPanel(){
-	String testCurrPlayerMessage = "First turn!";
-	String testNextPlayerMessage = "Next Player: \n" + testPlayers[0].getName();
-	String testPlayOrder[] = new String[testPlayers.length+1];
-	String actualPlayOrder[] = testPlayerPanel.getOrderText();
-	
-	testPlayOrder[0] = "Player Order: \n";
-	for (int i = 1; i <= testPlayers.length; i++){
-	    testPlayOrder[i] = i + ") " + testPlayers[i-1].getName();
-	}
+        String testCurrPlayerMessage = "First turn!";
+        String testNextPlayerMessage = "Next Player: \n" + testPlayers[0].getName();
+        String testPlayOrder[] = new String[testPlayers.length+1];
+        String actualPlayOrder[] = testPlayerPanel.getOrderText();
 
-	assertEquals(testCurrPlayerMessage, testPlayerPanel.getCurrPlayerText());
-	assertEquals(testNextPlayerMessage, testPlayerPanel.getNextPlayerText());
+        testPlayOrder[0] = "Player Order: \n";
+        for (int i = 1; i <= testPlayers.length; i++){
+            testPlayOrder[i] = i + ") " + testPlayers[i-1].getName();
+        }
 
-	for (int i = 0; i <= testPlayers.length; i++){
-	    assertEquals(testPlayOrder[i], actualPlayOrder[i]);
-	}
+        assertEquals(testCurrPlayerMessage, testPlayerPanel.getCurrPlayerText());
+        assertEquals(testNextPlayerMessage, testPlayerPanel.getNextPlayerText());
+
+        for (int i = 0; i <= testPlayers.length; i++){
+            assertEquals(testPlayOrder[i], actualPlayOrder[i]);
+        }
     }
 }
