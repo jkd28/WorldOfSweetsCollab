15bf692e70d3bc6044965c1549ba2d2be145bfe4
Benjamin Muscato <BenjaminMuscato@users.noreply.github.com>
2017-11-28 23:39:34 -0500

    Added serialVersionUID to Player class

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -1,6 +1,7 @@
 import java.io.Serializable;
 
 public class Player implements Serializable {
+    private static final long serialVersionUID = 1L;
     private String name;
     private BoardSpace position = null; //all players start at "0th" position according to write up
     private String token;
