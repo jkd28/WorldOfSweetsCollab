ff52bffa93f3869552310ca4343e739716f6311a
Brian Knotten <bjk69@pitt.edu>
2017-12-05 19:34:26 -0500

    Added Dad booleans in Player

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -5,8 +5,12 @@ public class Player implements Serializable {
     private String name;
     private BoardSpace position = null; //all players start at "0th" position according to write up
     private String token;
+    private boolean isDad = false;
     public Player(String n){
         name = n;
+	if(name.equals("Dad")){
+	    isDad = true;
+	}
     }
 
     //Use an instance of a boardspace object as position
@@ -23,9 +27,17 @@ public class Player implements Serializable {
     public String getName(){
         return name;
     }
+    
     public boolean setName(String n){
         name = n;
+	if(name.equals("Dad")){
+	    isDad = true;
+	}
         return true;
+    }
+
+    public boolean getDad(){
+	return isDad;
     }
 
     public String getToken(){
