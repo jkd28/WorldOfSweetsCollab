f81085438170d90228c07f87be2307fe8b2597ce
JD <jkd28@pitt.edu>
2017-12-06 10:26:08 -0500

    Add Player AI detection and accompanying tests

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -5,8 +5,19 @@ public class Player implements Serializable {
     private String name;
     private BoardSpace position = null; //all players start at "0th" position according to write up
     private String token;
+    private boolean isAI;
+
     public Player(String n){
         name = n;
+        setAI(name);
+    }
+
+    private void setAI(String n) {
+        if (n.equals("AI")) {
+            this.isAI = true;
+        } else {
+            this.isAI = false;
+        }
     }
 
     //Use an instance of a boardspace object as position
@@ -31,8 +42,13 @@ public class Player implements Serializable {
     public String getToken(){
       return token;
     }
+
     public boolean setToken(String t){
       token = t;
       return true;
+    }
+
+    public boolean isAI() {
+        return this.isAI;
     }
 }
